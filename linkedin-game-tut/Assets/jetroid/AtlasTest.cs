86be404662607dbc099852841390eaa651fbb740
Anthony Sams <anthony.sams77@gmail.com>
2022-10-20 18:59:07 -0500

    some kinda game i guess

---
+++ linkedin-game-tut/Assets/jetroid/AtlasTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+enum SpriteType { spikes }
+
+public class AtlasTest : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteType currentType;
+
+    private SpriteType lastType;
+
+    [SerializeField]
+    private SpriteAtlas atlas;
+
+    private SpriteRenderer myRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = GetComponent<SpriteRenderer>();
+        lastType = SpriteType.spikes;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeSprite();
+    }
+
+    private void ChangeSprite()
+    {
+        if (currentType != lastType)
+        {
+            myRenderer.sprite = atlas.GetSprite(currentType.ToString());
+
+            lastType = currentType;
+        }
+    }
+}
